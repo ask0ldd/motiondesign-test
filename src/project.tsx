3b8ba7d007dcb4312326d2954aea2e28f5ede1e4
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-06 22:52:38 +0100

    *edit* first 3 elts anim

--- src/project.tsx
+++ src/project.tsx
@@ -1,18 +1,21 @@
-import {makeProject} from '@revideo/core';
+import './global.css';
+import {easeInOutCubic, makeProject, map, tween} from '@revideo/core';
 
-import {Audio, Img, makeScene2D, Video} from '@revideo/2d';
+import {Audio, Img, Layout, makeScene2D, Rect, Txt, Video} from '@revideo/2d';
 import {all, chain, createRef, waitFor} from '@revideo/core';
 
 /**
  * The Revideo scene
  */
 const scene = makeScene2D('scene', function* (view) {
-  const logoRef = createRef<Img>();
+  // const logoRef = createRef<Img>();
+  const text = createRef<Txt>();
+  const smallBar = createRef<Rect>()
 
   yield view.add(
     <>
       <Video
-        src={'http://localhost:9000/public/stars.mp4'}
+        src={'http://localhost:9000/public/see.mp4'}
         size={['100%', '100%']}
         play={true}
       />
@@ -24,9 +27,45 @@ const scene = makeScene2D('scene', function* (view) {
     </>,
   );
 
-  yield* waitFor(1);
+  view.add(
+    <Rect fill="#ffffff" ref={smallBar} position={[-540 + 32, 0]} width={8} height={250} zIndex={3}></Rect>
+  )
+
+  yield tween(1.5, value => {
+    smallBar().scale.y(map(0, 1.5, easeInOutCubic(value)));
+  }),
+
+  yield* waitFor(0.5);
 
   view.add(
+    <Rect fill="#00000000" ref={text} padding={[24, 48]} position={[0, 0]} zIndex={2} layout>
+      <Layout direction="column">
+        <Txt
+          fill="#ffffff"
+          position={[0, 0]}
+          fontFamily="Inter"
+          lineHeight="120%"
+          fontWeight={800}
+          maxWidth={600}
+          grow={0}
+          width={600}
+          wrap="wrap"
+          textWrap
+        >
+          {"The Mediterranean Sea is an intercontinental sea stretching approximately 4,000 km between Europe, Africa, and Asia"}
+        </Txt>
+      </Layout>
+  </Rect>
+  );
+
+  yield* chain(
+    tween(2, value => {
+      text().position.x(map(-1000, -150, easeInOutCubic(value)));
+    }),
+    text().fill('rgba(0, 0, 0, 0.6)', 1)
+  )
+
+  /*view.add(
     <Img
       width={'1%'}
       ref={logoRef}
@@ -34,12 +73,12 @@ const scene = makeScene2D('scene', function* (view) {
         'https://revideo-example-assets.s3.amazonaws.com/revideo-logo-white.png'
       }
     />,
-  );
+  );*/
 
-  yield* chain(
+  /*yield* chain(
     all(logoRef().scale(40, 2), logoRef().rotation(360, 2)),
     logoRef().scale(60, 1),
-  );
+  );*/
 });
 
 /**
@@ -50,7 +89,7 @@ export default makeProject({
   settings: {
     // Example settings:
     shared: {
-      size: {x: 1920, y: 1080},
+      size: {x: 1080, y: 1920},
     },
   },
 });
