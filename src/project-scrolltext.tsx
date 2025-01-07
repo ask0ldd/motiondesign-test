33c1d22e815bb3ff5371d7118e09e7110edd99a4
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-07 12:25:31 +0100

    *edit* scrolltext project

--- src/project-scrolltext.tsx
+++ src/project-scrolltext.tsx
@@ -1,5 +1,5 @@
 import './global.css';
-import {easeInOutCubic, makeProject, map, tween} from '@revideo/core';
+import {delay, easeInOutCubic, makeProject, map, sequence, tween, useScene} from '@revideo/core';
 
 import {Audio, Img, Layout, makeScene2D, Rect, Txt, Video} from '@revideo/2d';
 import {all, chain, createRef, waitFor} from '@revideo/core';
@@ -9,8 +9,15 @@ import {all, chain, createRef, waitFor} from '@revideo/core';
  */
 const scene = makeScene2D('scene', function* (view) {
   // const logoRef = createRef<Img>();
-  const text = createRef<Txt>();
+  const textRect = createRef<Rect>()
+  const text = createRef<Txt>()
   const smallBar = createRef<Rect>()
+  const textLayout = createRef<Layout>()
+
+  const screenWidth = 1080
+  const screenHeight = 1920
+
+  const name = useScene().variables.get('username', 'new user');
 
   yield view.add(
     <>
@@ -28,20 +35,15 @@ const scene = makeScene2D('scene', function* (view) {
   );
 
   view.add(
-    <Rect fill="#ffffff" ref={smallBar} position={[-540 + 32, 0]} width={8} height={250} zIndex={3}></Rect>
+    <Rect fill="#ffffff" ref={smallBar} position={[-screenWidth / 2 + 32, 0]} width={8} height={250} zIndex={3}></Rect>
   )
 
-  yield tween(1.5, value => {
-    smallBar().scale.y(map(0, 1.5, easeInOutCubic(value)));
-  }),
-
-  yield* waitFor(0.5);
-
   view.add(
-    <Rect fill="#00000000" ref={text} padding={[24, 48]} position={[0, 0]} zIndex={2} layout>
-      <Layout direction="column">
+    <Rect fill="#00000000" ref={textRect} padding={[24, 48]} position={[-1000, 0]} zIndex={2} layout>
+      <Layout direction="column" ref={textLayout}>
         <Txt
           fill="#ffffff"
+          ref={text}
           position={[0, 0]}
           fontFamily="Inter"
           lineHeight="120%"
@@ -52,18 +54,35 @@ const scene = makeScene2D('scene', function* (view) {
           wrap="wrap"
           textWrap
         >
-          {"The Mediterranean Sea is an intercontinental sea stretching approximately 4,000 km between Europe, Africa, and Asia"}
+          {"The Mediterranean Sea is an intercontinental sea stretching approximately 4,000 km between Europe, Africa, and Asia."}
         </Txt>
       </Layout>
   </Rect>
   );
 
-  yield* chain(
+  yield tween(1.5, value => {
+    smallBar().height(map(0, textRect().height(), easeInOutCubic(value)));
+  }),
+
+  yield* waitFor(0.5);
+
+  yield* all(
     tween(2, value => {
-      text().position.x(map(-1000, -150, easeInOutCubic(value)));
+      textRect().position.x(map(-1000, (textRect().width() / 2) - screenWidth / 2 + 44, easeInOutCubic(value)));
     }),
-    text().fill('rgba(0, 0, 0, 0.6)', 1)
+    delay(0.5, textRect().fill('rgba(0, 0, 0, 0.5)', 1)),
+    // delay(2, text().fill('rgb(125, 0, 0)', 1))
   )
+
+  text().save()
+  // chain but with delay between actions
+  yield* sequence(0.3,
+    /*text().save(),
+    text().layout(false),*/
+    text().absolutePosition(-300, 1),
+  );
+
+  yield text().restore()
 
   /*view.add(
     <Img
@@ -93,3 +112,5 @@ export default makeProject({
     },
   },
 });
+
+// https://github.com/redotvideo/examples/tree/main/youtube-shorts
