5c9d032a874cf0962aebfaa3939de28955f7ca55
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-08 14:19:01 +0100

    *edit* txtblock component : open / close / unfold methods

--- src/components/TxtBlock.tsx
+++ src/components/TxtBlock.tsx
@@ -1,5 +1,5 @@
 import { NodeProps, Rect, Txt, Node, initial, colorSignal, signal } from "@revideo/2d";
-import { all, chain, ColorSignal, createRef, easeInOutCubic, range, Reference, Signal, SignalValue, SimpleSignal, Vector2 } from "@revideo/core";
+import { all, chain, ColorSignal, createRef, easeInOutCubic, easeInSine, range, Reference, Signal, SignalValue, SimpleSignal, Vector2 } from "@revideo/core";
 import '../global.css';
 
 export interface TxtBlockProps extends NodeProps {
@@ -7,7 +7,7 @@ export interface TxtBlockProps extends NodeProps {
 }
   
 export class TxtBlock extends Node {
-    @initial('#00000099')
+    @initial('#00000088')
     @colorSignal()
     public declare readonly backgroundColor: ColorSignal<this>;
   
@@ -22,6 +22,9 @@ export class TxtBlock extends Node {
     private mainContainer = createRef<Rect>();
     private lineContainers = range(5).map(rec => createRef<Rect>())
     private textLinesRefs = range(5).map(line => createRef<Txt>())
+    private lineContainersHeight : number
+    private mainContainersHeight : number
+    private mainContainersWidth : number
   
     public constructor(props?: TxtBlockProps) {
         super({ ...props, });
@@ -29,38 +32,73 @@ export class TxtBlock extends Node {
         this.add(
             <Rect opacity={0} fill={this.backgroundColor} padding={this.padding} ref={this.mainContainer} direction={'column'} rowGap={10} layout>
                 {props.textLines.map((textLine, index) => 
-                    <Rect fill={"#0000ff"} ref={this.lineContainers[index]} layout>
+                    <Rect ref={this.lineContainers[index]} layout>
                         <Txt ref={this.textLinesRefs[index]} fill={this.textColor}>{textLine}</Txt>
                     </Rect>)}
             </Rect>
         );
     }
 
-    public *init(){
+    public *init(open : boolean = true){
         this.mainContainer().save()
+        this.lineContainers.map(lc => lc().clip(true))
         this.lineContainers.forEach(lc => lc().save())
-        this.mainContainer().layout(false)
+        this.lineContainers.forEach(lc => lc().layout(false))
         this.mainContainer().restore()
-        /*this.lineContainers.forEach(lc => lc().layout(false))
         this.lineContainers.forEach(lc => lc().restore())
-        this.lineContainers.forEach(lc => lc().clip(true))*/
-        yield* this.lineContainers.map(lc => lc().layout(false))
-        yield* this.lineContainers.map(lc => lc().restore())
-        yield* this.lineContainers.map(lc => lc().clip(true))
+        this.mainContainer().layout(false)
+        this.lineContainersHeight = this.lineContainers[0]().height()
+        this.mainContainersHeight = this.mainContainer().height()
+        this.mainContainersWidth = this.mainContainer().width()
+        if(!open) {
+            this.lineContainers.forEach(container => container().height(0))
+            this.mainContainer().height(0)
+            this.mainContainer().width(0)
+            this.mainContainer().padding(0)
+        }
     }
 
     public *lineContainersClose(){
-        yield* chain(...this.lineContainers.map(lc => lc().height(0, 1)))
+        yield* chain(...this.lineContainers.map(lc => lc().height(0, 0.35, easeInSine)))
+    }
+
+    public *lineContainersOpen(){
+        if(this.lineContainersHeight) yield* chain(...this.lineContainers.map(lc => lc().height(this.lineContainersHeight, 0.35, easeInSine)))
     }
 
     public *show() {
-        yield* this.mainContainer().opacity(0, 0).to(1, 1);
+        yield* this.mainContainer().opacity(0, 0).to(1, 0.5, easeInSine);
     }
     
     public *hide() {
-        yield* this.mainContainer().opacity(1, 0).to(0, 1);
+        yield* this.mainContainer().opacity(1, 0).to(0, 1, easeInSine);
+    }
+
+    public *snapLeftBorder(viewWidth : number){
+        if(this.mainContainersWidth) this.mainContainer().position.x(- viewWidth/2 + this.mainContainersWidth/2)
     }
   
+    public *closeMainContainer(){
+        yield* chain(
+            all(
+                this.mainContainer().width(10, 1, easeInSine),
+                this.mainContainer().padding(0, 1, easeInSine)
+            ),
+            this.mainContainer().height(0, 0.5, easeInSine),
+        )
+    }
+
+    public *openMainContainer(){
+        if(this.mainContainersWidth && this.mainContainersHeight)
+        yield* chain(
+            all(
+                this.show(),
+                this.mainContainer().width(this.mainContainersWidth, 0.75, easeInSine),
+                this.mainContainer().padding(10, 0.75, easeInSine)
+            ),
+            this.mainContainer().height(this.mainContainersHeight, 0.5, easeInSine),
+        )
+    }
 }
 
 export default TxtBlock
