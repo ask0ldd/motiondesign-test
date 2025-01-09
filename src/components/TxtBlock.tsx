c3a88db65b4f06aedb8764e3aee20b968ec0adc8
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-09 02:44:07 +0100

    *edit* textblock refactor + new snapping & decorator related methods methods

--- src/components/TxtBlock.tsx
+++ src/components/TxtBlock.tsx
@@ -4,7 +4,12 @@ import '../global.css';
 
 export interface TxtBlockProps extends NodeProps {
     textLines : string[]
-    decorator? : boolean
+    decorator? : Side
+}
+
+export enum Side {
+    Left = "Left",
+    Right = "Right"
 }
   
 export class TxtBlock extends Node {
@@ -22,7 +27,7 @@ export class TxtBlock extends Node {
   
     private mainLayout = createRef<Layout>()
     private textContainer = createRef<Rect>()
-    private lineContainers = range(5).map(rec => createRef<Rect>())
+    // private lineContainers = range(5).map(rec => createRef<Rect>())
     private decorator = createRef<Rect>()
     private textLinesRefs = range(5).map(line => createRef<Txt>())
     private lineContainersHeight : number
@@ -35,16 +40,17 @@ export class TxtBlock extends Node {
         super({ ...props, });
     
         if(props.decorator) this.isDecorator = true
-        
+
         this.add(
             <Layout ref={this.mainLayout} gap={12} layout>
-                {props.decorator && <Rect ref={this.decorator} fill={"#ffffff"} width={12}/>}
+                {props.decorator == "Left" && <Rect ref={this.decorator} opacity={0} fill={"#ffffff"} width={12}/>}
                 <Rect opacity={1} fill={this.backgroundColor} padding={this.padding} ref={this.textContainer} direction={'column'} rowGap={10} layout>
                     {props.textLines.map((textLine, index) => 
-                        <Rect ref={this.lineContainers[index]} layout>
+                        <Rect layout>
                             <Txt ref={this.textLinesRefs[index]} lineHeight={'130%'} fontFamily={'Inter'} fontWeight={700} fontSize={40} fill={this.textColor}>{textLine}</Txt>
                         </Rect>)}
                 </Rect>
+                {props.decorator == "Right" && <Rect ref={this.decorator} opacity={0} fill={"#ffffff"} width={12}/>}
             </Layout>
         );
         this.mainLayoutWidth = this.mainLayout().width()
@@ -53,21 +59,26 @@ export class TxtBlock extends Node {
     public *init(open : boolean = true){
         this.textContainersHeight = this.textContainer().height()
         this.textContainersWidth = this.textContainer().width()
-        this.lineContainersHeight = this.lineContainers[0]().height() + 4 // choose the linecontainer with the biggest height instead
+        // this.lineContainersHeight = this.lineContainers[0]().height() + 4 // choose the linecontainer with the biggest height instead
+        this.lineContainersHeight = (this.textContainer().children()[0] as Rect).height() + 4
         if(this.isDecorator) this.decorator().save()
-        this.textContainer().save()
-        this.lineContainers.forEach(lc => {
-            lc().clip(true)
-            lc().save()
-        })
-        this.lineContainers.forEach(lc => lc().layout(false))
-        this.textContainer().restore()
-        this.lineContainers.forEach(lc => lc().restore())
+        this.textContainer().save();
+        // this.lineContainers.forEach(lc => {
+        (this.textContainer().children() as Rect[]).forEach(rect => {
+            rect.clip(true);
+            rect.save()
+        });
+        // this.lineContainers.forEach(lc => lc().layout(false))
+        (this.textContainer().children() as Rect[]).forEach(rect => rect.layout(false))
+        this.textContainer().restore();
+        // this.lineContainers.forEach(lc => lc().restore())
+        (this.textContainer().children() as Rect[]).forEach(rect => rect.restore())
         this.textContainer().layout(false)
         this.mainLayout().layout(false)
         if(this.isDecorator) this.decorator().restore()
         if(!open) {
-            this.lineContainers.forEach(container => container().height(0))
+            // this.lineContainers.forEach(container => container().height(0))
+            (this.textContainer().children() as Rect[]).forEach(rect => rect.height(0))
             this.textContainer().height(0)
             this.textContainer().width(0)
             this.textContainer().padding(0)
@@ -76,11 +87,16 @@ export class TxtBlock extends Node {
     }
 
     public *foldLineContainers(){
-        yield* chain(...this.lineContainers.map(lc => lc().height(0, 0.2, easeInSine)))
+        yield* all(
+            chain(...(this.textContainer().children() as Rect[]).map(rect => rect.height(0, 0.2, easeInSine))),
+            // chain(...(this.textContainer().children() as Rect[]).map(rect => rect.position.x(300, 0.2, easeInSine))),
+            chain(...(this.textContainer().children() as Rect[]).map(rect => rect.opacity(0, 0.2, easeInSine))),
+        )
     }
 
     public *unfoldLineContainers(){
-        if(this.lineContainersHeight) yield* chain(...this.lineContainers.map(lc => lc().height(this.lineContainersHeight, 0.35, easeInSine)))
+        if(this.lineContainersHeight) yield* chain(...(this.textContainer().children() as Rect[]).map(rect => rect.height(this.lineContainersHeight, 0.35, easeInSine)))
+        
     }
 
     public *show() {
@@ -94,27 +110,39 @@ export class TxtBlock extends Node {
     public *snapLeftBorder(marginLeft : number = 0, viewWidth : number){
         if(this.mainLayoutWidth) this.mainLayout().position.x(- viewWidth/2 + this.mainLayoutWidth/2 + marginLeft)
     }
+
+    public *snapRightBorder(marginRight : number = 0, viewWidth : number){
+        if(this.mainLayoutWidth) this.mainLayout().position.x(+ viewWidth/2 - this.mainLayoutWidth/2 - marginRight)
+    }
   
     public *closeTextContainer(){
         yield* chain(
             all(
                 this.textContainer().width(10, 0.35, easeInSine),
                 this.textContainer().padding(0, 0.35, easeInSine),
-                this.isDecorator ? this.decorator().position.x(-1000, 0.15, easeInSine) : void 0
+                // this.isDecorator ? this.decorator().position.x(-1000, 0.15, easeInSine) : void 0
             ),
             this.textContainer().height(0, 0.25, easeInSine),
         )
     }
 
+    public *decoratorDisappearLeft(){
+        yield* this.decorator().position.x(-1000, 0.15, easeInSine)
+        this.decorator().remove()
+    }
+
     public *openTextContainer(){
         if(this.textContainersWidth && this.textContainersHeight)
-        yield* chain(
+        yield* all(
+            chain(
             all(
                 this.show(),
                 this.textContainer().width(this.textContainersWidth, 0.75, easeInSine),
-                this.textContainer().padding(10, 0.75, easeInSine)
+                this.textContainer().padding(10, 0.75, easeInSine),
             ),
             this.textContainer().height(this.textContainersHeight, 0.5, easeInSine),
+            ),
+            this.decorator().opacity(1, 1.25)
         )
     }
 }
